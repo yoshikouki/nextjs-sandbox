35e6b594eeb63d84a667c25028787e388eeaa036
yoshikouki <yoshikouki@gmail.com>
2023-10-19 21:37:35 +0900

    rm -rf ./*

--- src/app/[text]/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Text({ params }: { params: { text: string } }) {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {params.text}
-    </main>
-  );
-}
